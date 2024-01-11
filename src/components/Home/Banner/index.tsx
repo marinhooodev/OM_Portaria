bfb94dc21915140550ed8234fddbf0e09e521cb2
marinhooodev <gabriel.anjo722@gmail.com>
2024-01-11 14:17:26 -0300

    Banner Fix

--- src/components/Home/Banner/index.tsx
+++ src/components/Home/Banner/index.tsx
@@ -5,10 +5,10 @@ const Banner = () => {
   return (
     <div className="bannerIndex">
       <div className="content">
-        <h1>Segurança muito foda é aqui foda-se</h1>
+        <h1>Segurançaaaaaaaaaaaaa</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum explicabo sit, est amet, minima pariatur ex, magni quis neque id aliquam facilis ad laudantium sunt quibusdam icta.</p>
 
-        <a href="#">Botao do meu saco</a>
+        <a href="#">Botao botao</a>
       </div>
     </div>
   );
